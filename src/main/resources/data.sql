4fc7ec944b3cd647505c6ec5bbbe720f3c039001
Stas Tarasov <iluvstrwrs@mail.ru>
2020-03-03 21:19:40 +0300

    added main functionality

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO app_user (authority, email, enabled , username, password, first_name, last_name) VALUES ('ROLE_ADMIN', 'EMAIL1@MAIL.RU','true','USERNAME1','$2a$10$4fR.sc.W92v.UqltCmUIcu1IkMtncGNfmIBk/hsciTcNA2Af68w46','FIRST NAME1', 'LAST NAME1');
+
+INSERT INTO room (area) VALUES (123);

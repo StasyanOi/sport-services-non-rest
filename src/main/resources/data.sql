51d5f9bc607fa963a0282f0312c7c22e3726da47
Stas Tarasov <iluvstrwrs@mail.ru>
2020-05-05 13:52:03 +0300

    changed base usernames

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('Admin','ROLE_ADMIN', 'EMAIL1@MAIL.RU','true','USERNAME1','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME1', 'LAST NAME1');
-INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('Learner','ROLE_USER', 'EMAIL2@MAIL.RU','true','USERNAME2','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME2', 'LAST NAME2');
-INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('Trainer','ROLE_TRAINER', 'EMAIL3@MAIL.RU','true','USERNAME3','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME3', 'LAST NAME3');
-INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('SecurityUser','ROLE_SECURITY', 'EMAIL4@MAIL.RU','true','USERNAME4','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME4', 'LAST NAME4');
+INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('Admin','ROLE_ADMIN', 'EMAIL1@MAIL.RU','true','ADMIN','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME1', 'LAST NAME1');
+INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('Learner','ROLE_USER', 'EMAIL2@MAIL.RU','true','LEARNER','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME2', 'LAST NAME2');
+INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('Trainer','ROLE_TRAINER', 'EMAIL3@MAIL.RU','true','TRAINER','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME3', 'LAST NAME3');
+INSERT INTO app_user (DTYPE, authority, email, enabled , username, password, first_name, last_name) VALUES ('SecurityUser','ROLE_SECURITY', 'EMAIL4@MAIL.RU','true','SECURITY','$2a$10$UX9Lvz3dyPt169Ebv8COs.EtWMPd01Myes.pVZel5ORfSrIQrDk8i','FIRST NAME4', 'LAST NAME4');
 
 INSERT INTO room (area) VALUES (123);
 INSERT INTO room_request (requester_id, room_id, approved_admin,approved_security

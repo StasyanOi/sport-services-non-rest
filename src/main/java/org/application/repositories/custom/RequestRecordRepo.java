3026e4274fe42ea7d97546ce13b4e57d6a2bc72c
Stas Tarasov <iluvstrwrs@mail.ru>
2020-05-23 21:41:48 +0300

    custom dao

--- src/main/java/org/application/repositories/custom/RequestRecordRepo.java
+++ src/main/java/org/application/repositories/custom/RequestRecordRepo.java
@@ -2,7 +2,25 @@ package org.application.repositories.custom;
 
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.sql.DriverManager;
+
 @Repository
 public class RequestRecordRepo {
+
+    private String jdbcUrl = "jdbc:h2:mem:db";
+    private String username = "sa";
+    private String password = "";
+
+    private DriverManager driverManager;
+
+    @PostConstruct
+    public void init(){
+    }
+
+    @PreDestroy
+    public void destroy(){
+    }
 
 }
